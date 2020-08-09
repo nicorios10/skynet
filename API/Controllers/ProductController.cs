7d1203bf74ea7dcfe3be9faf7fa2a9f6ee519bc9
Nicolas Rios <nrios@baufest.com>
2020-08-09 15:35:23 -0300

    Initial commit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        [Route("products")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("getProduct/{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+
+    }
+}
